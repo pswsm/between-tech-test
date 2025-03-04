f57d16e3232dc333567adea1e0c7480ede40ca1c
pswsm <53122461+pswsm@users.noreply.github.com>
2025-03-04 13:58:27 +0100

    refactor: use image variant class

--- src/contexts/tasks/domain/Image.ts
+++ src/contexts/tasks/domain/Image.ts
@@ -1,27 +1,28 @@
 import { StringValueObject } from 'src/shared/StringValueObject';
 import { Path } from './value-object/Path';
-import { Resolution } from './value-object/Resolution';
 import { PrimitiveOf } from 'src/shared/FromPrimitves';
+import { Variant } from './value-object/Variant';
 
 export class Image {
   public static fromPrimitives(primitives: PrimitiveOf<Image>): Image {
     return new Image(
       new StringValueObject(primitives.id),
       new Path(primitives.path),
-      primitives.resolutions.map((r) => new Resolution(r)),
+      primitives.variants?.map((r) => Variant.fromPrimitives(r)),
     );
   }
+
   constructor(
     private readonly id: StringValueObject,
     private readonly path: Path,
-    private readonly resolutions: Resolution[],
+    private readonly variants?: Variant[],
   ) {}
 
   public toPrimitives() {
     return {
       id: this.id.valueOf(),
       path: this.path.valueOf(),
-      resolutions: this.resolutions.map((r) => r.valueOf()),
+      variants: this.variants?.map((v) => v.toPrimitives()),
     };
   }
 }
