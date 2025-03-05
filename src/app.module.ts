bd97efd9960704b48e60936b291a26bfbe915b79
pswsm <53122461+pswsm@users.noreply.github.com>
2025-03-05 19:27:02 +0100

    feat: connect apps and controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TaskController } from './controllers/TaskController';
+import { TaskModule } from './contexts/tasks/task.module';
+import { ImageModule } from './contexts/images/images.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TaskModule, ImageModule],
+  controllers: [TaskController, AppController],
   providers: [AppService],
 })
 export class AppModule {}
