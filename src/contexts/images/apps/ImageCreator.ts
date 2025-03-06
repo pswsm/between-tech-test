2a40043c42055205b7d01b4b73effe6c78ffcc2a
pswsm <53122461+pswsm@users.noreply.github.com>
2025-03-06 18:38:27 +0100

    refactor: update image creator to domain

--- src/contexts/images/apps/ImageCreator.ts
+++ src/contexts/images/apps/ImageCreator.ts
@@ -1,9 +1,16 @@
+import { MD5Hash } from 'src/shared/MD5Hash';
 import { Image } from '../domain/Image';
 import { ImageId } from '../domain/value-objects/ImageId';
 import { Path } from '../domain/value-objects/Path';
+import { Timestamp } from 'src/shared/Timestamp';
 
 export default class ImageCreator {
   public static create(imagePath: Path): Image {
-    return new Image(ImageId.new(), imagePath);
+    return new Image(
+      ImageId.new(),
+      imagePath,
+      MD5Hash.new(imagePath.valueOf()),
+      Timestamp.now(),
+    );
   }
 }
