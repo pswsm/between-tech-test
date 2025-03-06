718bb9e2abfe45cb2480d67b02dc17d1da6c74f5
pswsm <53122461+pswsm@users.noreply.github.com>
2025-03-06 20:30:32 +0100

    refactor: allow binary hashing

--- src/shared/MD5Hash.ts
+++ src/shared/MD5Hash.ts
@@ -1,8 +1,8 @@
-import { createHash, type Hash } from 'node:crypto';
+import { BinaryLike, createHash, type Hash } from 'node:crypto';
 import { StringValueObject } from './StringValueObject';
 
 export class MD5Hash extends StringValueObject {
-  public static new(value: string): MD5Hash {
+  public static new(value: BinaryLike): MD5Hash {
     const hasher: Hash = createHash('md5');
     const hashedValue: string = hasher.update(value).digest('hex');
     return new MD5Hash(hashedValue);
