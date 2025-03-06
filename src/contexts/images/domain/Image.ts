8bac302cd9d69196a064fd7e79b6b641c99c0382
pswsm <53122461+pswsm@users.noreply.github.com>
2025-03-06 17:26:53 +0100

    feat: add hash and created time to image domain

--- src/contexts/images/domain/Image.ts
+++ src/contexts/images/domain/Image.ts
@@ -3,12 +3,16 @@ import { PrimitiveOf } from 'src/shared/FromPrimitves';
 import { AggregateRoot } from 'src/shared/AggregateRoot';
 import { ImageId } from './value-objects/ImageId';
 import { Resolution } from './value-objects/Resolution';
+import { Timestamp } from 'src/shared/Timestamp';
+import { MD5Hash } from 'src/shared/MD5Hash';
 
 export class Image extends AggregateRoot {
   public static fromPrimitives(primitives: PrimitiveOf<Image>): Image {
     return new Image(
       new ImageId(primitives.id),
       new Path(primitives.path),
+      new MD5Hash(primitives.hash),
+      new Timestamp(primitives.createdAt),
       primitives.resolution ? new Resolution(primitives.resolution) : undefined,
     );
   }
@@ -16,6 +20,8 @@ export class Image extends AggregateRoot {
   constructor(
     id: ImageId,
     private readonly path: Path,
+    private readonly hash: MD5Hash,
+    private readonly createdAt: Timestamp,
     private readonly resolution?: Resolution,
   ) {
     super(id);
@@ -37,6 +43,8 @@ export class Image extends AggregateRoot {
     return {
       id: this.id.valueOf(),
       path: this.path.valueOf(),
+      hash: this.hash.valueOf(),
+      createdAt: this.createdAt.valueOf(),
       resolution: this.resolution?.valueOf(),
     };
   }
