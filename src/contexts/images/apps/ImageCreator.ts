687df93e89da19d00c20f7c422ac823001429d9f
pswsm <53122461+pswsm@users.noreply.github.com>
2025-03-07 00:53:29 +0100

    refactor: image creator to accommodate case of path not being local

--- src/contexts/images/apps/ImageCreator.ts
+++ src/contexts/images/apps/ImageCreator.ts
@@ -5,17 +5,46 @@ import { Path } from '../domain/value-objects/Path';
 import { Timestamp } from 'src/shared/Timestamp';
 import { ImageRepository } from '../domain/ImageRepository';
 import { Inject, Injectable } from '@nestjs/common';
+import { readFile, writeFile } from 'node:fs/promises';
+import { UnreadableImage } from './errors/UnreadableImage';
+import { DownloadFolder } from './DownloadFolder';
 
 @Injectable()
 export default class ImageCreator {
   constructor(
     @Inject('ImageRepository') private readonly repository: ImageRepository,
   ) {}
-  public async create(imagePath: Path, hash: MD5Hash): Promise<Image> {
-    const image = new Image(ImageId.new(), imagePath, hash, Timestamp.now());
+  public async create(imagePath: Path): Promise<Image> {
+    if (imagePath.isRemote()) {
+      return await this.createRemoteImage(imagePath);
+    }
+    return await this.createLocalImage(imagePath);
+  }
 
-    await this.repository.insert(image);
+  private async createLocalImage(imagePath: Path): Promise<Image> {
+    let buffer: Buffer;
+    try {
+      buffer = await readFile(imagePath.valueOf());
+    } catch {
+      throw new UnreadableImage(imagePath);
+    }
+    const hash = MD5Hash.new(buffer);
+    return new Image(ImageId.new(), imagePath, hash, Timestamp.now());
+  }
 
-    return image;
+  private async createRemoteImage(imagePath: Path): Promise<Image> {
+    const responseBody = await fetch(imagePath.valueOf()).then((res) => {
+      if (!res.ok) {
+        throw new UnreadableImage(imagePath);
+      }
+      return res.arrayBuffer();
+    });
+    const buf = Buffer.from(responseBody);
+    const hash = MD5Hash.new(buf);
+    await writeFile(
+      `${DownloadFolder}/${hash.valueOf()}.${imagePath.getExtension().valueOf()}`,
+      buf,
+    );
+    return new Image(ImageId.new(), imagePath, hash, Timestamp.now());
   }
 }
