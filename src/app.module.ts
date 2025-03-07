e78b44b7a838109daa219eb8803b6ed052d7df21
pswsm <53122461+pswsm@users.noreply.github.com>
2025-03-07 04:21:58 +0100

    chore: import modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { ImageModule } from './contexts/images/images.module';
 import { GetTaskController } from './controllers/GetTaskController';
 import { PostTaskController } from './controllers/PostTaskController';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { EventHandlersModule } from './event-handlers/handlers.module';
 
 @Module({
-  imports: [TaskModule, ImageModule, EventEmitterModule.forRoot()],
+  imports: [
+    TaskModule,
+    ImageModule,
+    EventEmitterModule.forRoot(),
+    EventHandlersModule,
+  ],
   controllers: [GetTaskController, PostTaskController],
   providers: [],
 })
